127406a95a5cdfe919db302400c12fa7f5e1996a
bamlak <bamlak996@gmail.com>
2023-10-04 07:09:32 -0700

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,8 +3,8 @@
 
 /**
  * create_array - create array of size size and assign char c		
- * @size:  size of array
- * @c:  char to assign
+ * @size: size of array
+ * @c: char to assign
  * Description: create array of size size and assign char c
  *
  * Return: pointer to array, NULL if fail		

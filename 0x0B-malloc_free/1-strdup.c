4a07d8ba43589142a8ed80ae4caf874340885a63
bamlak <bamlak996@gmail.com>
2023-10-04 07:30:49 -0700

    0

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,18 +2,12 @@
 		
 #include <stdlib.h>
 		
-#include "main.h"
-		
-/**
-		
- * _strdup - duplicate to new memory space location
-		
- * @str: char
-		
- * Return: 0
-		
- */
-		
+#include "main.h"		
+/**		
+ * _strdup - duplicate to new memory space location		
+ * @str: char		
+ * Return: 0		
+ */		
 char *_strdup(char *str)
 		
 {
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,16 +2,11 @@
 		
 #include <stdlib.h>
 		
-/**
-		
- * alloc_grid - nested loop to make grid
-		
- * @width: width input
-		
- * @height: height input
-		
- * Return: pointer to 2 dim. array
-		
+/**		
+ * alloc_grid - nested loop to make grid		
+ * @width: width input		
+ * @height: height input	
+ * Return: pointer to 2 dim. array		
  */
 		
 int **alloc_grid(int width, int height)
